ba26655e2ef86c0284ad5a0ce885ce55bf25ca4d
harvyml <harvyestebanml@gmail.com>
2022-08-21 16:44:32 -0500

    refactor: improved project structure

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -2,22 +2,17 @@ import React, { useState } from 'react';
 import { useEffect } from 'react';
 
 function App() {
-  const [state, setState] = useState(0)
+  const [state, setState] = useState(0);
 
-  function handleClick(){
-    setState(current => current+1)
+  function handleClick() {
+    setState((current) => current + 1);
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          count {state}
-        </p>
-        <a
-          className="App-link"
-          onClick={handleClick}
-        >
+        <p>count {state}</p>
+        <a className="App-link" onClick={handleClick}>
           Increment
         </a>
       </header>
