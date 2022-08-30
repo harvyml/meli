e37d52dd41d4c6bc8512c565d8bcc2c1bfd3c3c2
harvyml <harvyestebanml@gmail.com>
2022-08-30 09:09:36 -0500

    feat: added status codes for relaibality

--- src/server/src/api/index.js
+++ src/server/src/api/index.js
@@ -4,13 +4,27 @@ import { productRequest, productsRequest } from '../services';
 const api = Router();
 
 api.get('/items', async (req, res) => {
-  let request = await productsRequest(req.query.limit || 4, req.query.search);
-  res.json(request);
+  try {
+    let request = await productsRequest(req.query.limit || 4, req.query.search);
+    res.status(200).json(request);
+  } catch (err) {
+    res.status(500).json({
+      err,
+      ok: false,
+    });
+  }
 });
 
 api.get('/items/:id', async (req, res) => {
-  let request = await productRequest(req.params.id);
-  res.json(request);
+  try {
+    let request = await productRequest(req.params.id);
+    res.status(200).json(request);
+  } catch (err) {
+    res.status(500).json({
+      err,
+      ok: false,
+    });
+  }
 });
 
 export default api;
